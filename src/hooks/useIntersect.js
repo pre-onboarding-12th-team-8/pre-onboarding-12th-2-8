7e2308b2dedf55485562ab91f7e49bd64d921ae6
이종혁 <dlwhd5717@naver.com>
2023-08-31 23:04:08 +0900

    feat : useIntersect hook 추가

---
+++ src/hooks/useIntersect.js
@@ -0,0 +1,27 @@
+import { useRef, useCallback, useEffect } from "react";
+export const useIntersect = (onIntersectCallback) => {
+  const ref = useRef(null);
+  const onIntersect = useCallback(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          onIntersectCallback(entry, observer);
+        }
+      });
+    },
+    [onIntersectCallback]
+  );
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    const observer = new IntersectionObserver(onIntersect, {
+      threshold: 0.9,
+    });
+    observer.observe(ref.current);
+
+    return () => observer && observer.disconnect();
+  }, [ref, onIntersect]);
+  return ref;
+};
