1edb7c75c348a2fdcca6214011a71882ad3488b8
Park jinyoung <asd029600@gmail.com>
2023-09-01 16:46:29 +0900

    add : 홈페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
+import { Home } from "./pages/Home";
 import { Issues } from "./pages/Issues/Issues";
 import { DetailIssue } from "./pages/Issue/DetailIssue";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<div></div>}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/issues" element={<Issues />}></Route>
           <Route path="/issues/:id" element={<DetailIssue />}></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
